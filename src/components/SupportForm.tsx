908e63f4d794141899192fc2b49ad5eebbdf9c63
Alexey Vlasov <sketchrhc@yandex.ru>
2024-08-31 20:15:36 +0300

    bg dark

--- src/components/SupportForm.tsx
+++ src/components/SupportForm.tsx
@@ -47,38 +47,38 @@ const SupportForm = () => {
     return (
         <div className="mb-8 px-2">
             <h2 className="text-2xl font-bold mb-6">Задать вопрос</h2>
-            <div className="p-6 bg-white rounded-lg shadow-md">
+            <div className="p-6 bg-white rounded-lg shadow-md dark:bg-gray-700">
                 <div className="max-w-md mx-auto ">
 
                     {success && <p className="text-green-500 mb-4">Ваше сообщение отправлено!</p>}
                     {error && <p className="text-red-500 mb-4">{error}</p>}
                     <form onSubmit={handleSubmit}>
                         <div className="mb-4">
-                            <label className="block mb-2 text-sm font-medium text-gray-700">Имя</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">Имя</label>
                             <input
                                 type="text"
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
-                                className="border rounded-md p-2 w-full"
+                                className="border rounded-md p-2 w-full dark:text-black"
                                 required
                             />
                         </div>
                         <div className="mb-4">
-                            <label className="block mb-2 text-sm font-medium text-gray-700">Email</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">Email</label>
                             <input
                                 type="email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
-                                className="border rounded-md p-2 w-full"
+                                className="border rounded-md p-2 w-full dark:text-black"
                                 required
                             />
                         </div>
                         <div className="mb-4">
-                            <label className="block mb-2 text-sm font-medium text-gray-700">Сообщение</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">Сообщение</label>
                             <textarea
                                 value={message}
                                 onChange={(e) => setMessage(e.target.value)}
-                                className="border rounded-md p-2 w-full"
+                                className="border rounded-md p-2 w-full dark:text-black"
                                 rows={4}
                                 required
                             />
